960842e27bc016d46f0712ea9792a7118368719b
Michael <1372930+dodwmd@users.noreply.github.com>
2024-11-05 09:55:23 +1000

    update formatting and fix checkov

--- modules/wizardai_aws_s3_bucket/main.tf
+++ modules/wizardai_aws_s3_bucket/main.tf
@@ -1,11 +1,11 @@
 # main.tf
 
 # Create the S3 bucket
-# checkov:skip=CKV2_AWS_62:Out of scope for this demo
-# checkov:skip=CKV_AWS_18:Out of scope for this demo
-# checkov:skip=CKV_AWS_144:Out of scope for this demo
 resource "aws_s3_bucket" "this" {
-  bucket = format("wizardai-%s-%s", lower(var.name), lower(var.environment))
+  # checkov:skip=CKV2_AWS_62:Out of scope for this demo
+  # checkov:skip=CKV_AWS_18:Out of scope for this demo
+  # checkov:skip=CKV_AWS_144:Out of scope for this demo
+  bucket        = format("wizardai-%s-%s", lower(var.name), lower(var.environment))
   force_destroy = var.force_destroy
 
   # Tags
