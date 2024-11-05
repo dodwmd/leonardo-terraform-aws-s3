74221ee4d9c7616e6861b632d08985206ac33a02
Michael <1372930+dodwmd@users.noreply.github.com>
2024-11-05 11:23:38 +1000

    make the bucketname unique

--- tests/s3_bucket_test.go
+++ tests/s3_bucket_test.go
@@ -1,40 +1,45 @@
-	// tests/s3_bucket_test.go
-	package tests
+// tests/s3_bucket_test.go
+package tests
 
-	import (
-		"testing"
+import (
+	"testing"
 
-		"github.com/gruntwork-io/terratest/modules/terraform"
-		"github.com/stretchr/testify/assert"
-	)
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
 
-	func TestS3BucketModule(t *testing.T) {
-		t.Parallel()
+func TestS3BucketModule(t *testing.T) {
+	t.Parallel()
 
-		// Define Terraform options
-		terraformOptions := &terraform.Options{
-			// Path to the Terraform code that will be tested
-			TerraformDir: "../modules/wizardai_aws_s3_bucket",
+	// Generate a unique bucket name by appending a random suffix
+	randomID := random.UniqueId()
+	bucketName := "example-" + randomID
 
-			// Variables to pass to our Terraform code using the terraform.tfvars file
-			Vars: map[string]interface{}{
-				"name": "example",
-				"environment": "development",
-			},
+	// Define Terraform options
+	terraformOptions := &terraform.Options{
+		// Path to the Terraform code that will be tested
+		TerraformDir: "../modules/wizardai_aws_s3_bucket",
 
-			// Set AWS region
-			EnvVars: map[string]string{
-				"AWS_REGION": "ap-southeast-2",
-			},
+		// Variables to pass to our Terraform code using the terraform.tfvars file
+		Vars: map[string]interface{}{
+			"name":        bucketName,
+			"environment": "development",
+		},
+
+		// Set AWS region
+		EnvVars: map[string]string{
+			"AWS_REGION": "ap-southeast-2",
+		},
 	}
 
-		// Clean up resources with "terraform destroy" at the end of the test
-		defer terraform.Destroy(t, terraformOptions)
+	// Clean up resources with "terraform destroy" at the end of the test
+	defer terraform.Destroy(t, terraformOptions)
 
-		// Run "terraform init" and "terraform apply"
-		terraform.InitAndApply(t, terraformOptions)
+	// Run "terraform init" and "terraform apply"
+	terraform.InitAndApply(t, terraformOptions)
 
-		// Run test checks (e.g., ensure bucket id is correctly set)
-		bucketID := terraform.Output(t, terraformOptions, "bucket_id")
-		assert.Equal(t, bucketID, "wizardai-example-development")
-	}
+	// Run test checks (e.g., ensure bucket id is correctly set)
+	bucketID := terraform.Output(t, terraformOptions, "bucket_id")
+	assert.Equal(t, bucketID, "wizardai-"+bucketName+"-development")
+}
